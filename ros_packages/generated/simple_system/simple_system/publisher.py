31ac345837b74f295db8d1dedb860c2760d5387d
Anna <annadiplacido3@gmail.com>
2025-05-15 22:49:14 +0200

    add simple publisher exemple

---
+++ ros_packages/generated/simple_system/simple_system/publisher.py
@@ -0,0 +1,35 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import ExternalShutdownException
+from std_msgs.msg import String, Int32, Float32, Bool, Header
+from simple_system.timer_execution import measure_execution_time
+
+class PublisherNode(Node):
+    def __init__(self):
+        super().__init__('Publisher')
+        self.declare_parameter('rate', 1000)
+        self.mode = 'idle'
+        self.pub_timer_timer = self.create_timer(1.000, self.pub_timer_callback)
+
+
+    @measure_execution_time()
+    def on_timerElapsed_pub_timer(self):
+        if not hasattr(self, 'publisher_data_topic'):
+            self.publisher_data_topic = self.create_publisher(String, 'data_topic', 10)
+        self.publisher_data_topic.publish(String(data='Hello from publisher'))
+    
+    def pub_timer_callback(self):
+        self.on_timerElapsed_pub_timer()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PublisherNode()
+    try:
+        rclpy.spin(node)
+    except (KeyboardInterrupt, ExternalShutdownException):
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.try_shutdown()
+    
