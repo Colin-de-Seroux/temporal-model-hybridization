579ec064d482d74998d882ca5798b87a95da45bf
Colin-de-Seroux <77831261+Colin-de-Seroux@users.noreply.github.com>
2025-05-18 17:29:58 +0200

    :construction: Add nodes and callback functions

--- ros_aml/src/cli/generator_files/graph_generator.ts
+++ ros_aml/src/cli/generator_files/graph_generator.ts
@@ -1,4 +1,9 @@
-import { type Model } from '../../language/generated/ast.js';
+import {
+    type CallbackFunction,
+    type Model,
+    type Node,
+    type Step,
+} from '../../language/generated/ast.js';
 
 export function generateJsonGraphModel(model: Model): string {
     console.log(model);
@@ -9,14 +14,46 @@ export function generateJsonGraphModel(model: Model): string {
     graph['nodes'] = [];
 
     model.nodes.forEach((node) => {
-        const jsonNode: any = {};
-        jsonNode['name'] = node.name;
-        jsonNode['expectedExecTime'] = node.expectedExecTime;
-
-        graph['nodes'].push(jsonNode);
+        graph['nodes'].push(createNode(node));
     });
 
     const graphString = JSON.stringify(graph, null, 4);
 
     return graphString;
+}
+
+function createNode(node: Node): any {
+    const jsonNode: any = {};
+    jsonNode['name'] = node.name;
+    jsonNode['expectedExecTime'] = node.expectedExecTime;
+
+    jsonNode['init'] = [];
+    node.init?.steps.forEach((step) => {
+        jsonNode['init'].push(createStep(step));
+    });
+
+    jsonNode['callbackFunctions'] = [];
+    node.callbackFunctions?.forEach((callback) => {
+        jsonNode['callbackFunctions'].push(createCallbackFunction(callback));
+    });
+
+    return jsonNode;
+}
+
+function createCallbackFunction(callback: CallbackFunction): any {
+    const jsonCallback: any = {};
+    jsonCallback['name'] = callback.name;
+    jsonCallback['type'] = callback.type;
+
+    jsonCallback['steps'] = [];
+    callback.steps?.steps.forEach((step) => {
+        jsonCallback['steps'].push(createStep(step));
+    });
+
+    return jsonCallback;
+}
+
+function createStep(step: Step): any {
+    // TODO
+    return {};
 }
