1c7eb5a28560f3c9d5de08e9c90d1ee572623c29
Colin-de-Seroux <77831261+Colin-de-Seroux@users.noreply.github.com>
2025-06-03 15:56:35 +0200

    :sparkles: Json graph v1 finish

--- ros_aml/src/cli/generator_files/json_generator.ts
+++ ros_aml/src/cli/generator_files/json_generator.ts
@@ -1,8 +1,7 @@
 import {
-    Trigger,
+    type Action,
     type Behavior,
     type Model,
-    type Timer,
 } from '../../language/generated/ast.js';
 
 export function generateJsonModel(model: Model): string {
@@ -15,12 +14,7 @@ export function generateJsonModel(model: Model): string {
         const jsonNode: any = {};
         jsonNode['name'] = node.name;
         jsonNode['expectedExecTime'] = Number(node.expectedExecTime);
-        jsonNode['timers'] = [];
         jsonNode['behaviors'] = [];
-
-        for (const timer of node.timers) {
-            jsonNode['timers'].push(generateTimer(timer));
-        }
 
         for (const behavior of node.behaviors) {
             jsonNode['behaviors'].push(generateBehavior(behavior));
@@ -34,38 +28,42 @@ export function generateJsonModel(model: Model): string {
     return graphString;
 }
 
-function generateTimer(timer: Timer): any {
-    const jsonTimer: any = {};
-    jsonTimer['name'] = timer.name;
-    jsonTimer['period'] = Number(timer.period);
-
-    return jsonTimer;
-}
-
 function generateBehavior(behavior: Behavior): any {
-    const jsonBehavior: any = {};
-    jsonBehavior['trigger'] = generateTrigger(behavior.trigger);
+    let jsonBehavior: any = { actions: [] };
+
+    if (behavior.trigger.$type === 'TimerElapsed') {
+        jsonBehavior['actions'].push({
+            type: 'timer',
+            value: behavior.trigger.timer,
+        });
+    } else if (behavior.trigger.$type === 'MessageReceived') {
+        jsonBehavior['actions'].push({
+            type: 'sub',
+            topic: behavior.trigger.topic,
+        });
+    }
+
+    for (const action of behavior.action) {
+        const jsonAction = generateAction(action);
+
+        if (jsonAction !== undefined) {
+            jsonBehavior['actions'].push(jsonAction);
+        }
+    }
 
     return jsonBehavior;
 }
 
-function generateTrigger(trigger: Trigger): any {
-    const jsonTrigger: any = {};
-    jsonTrigger['type'] = trigger.$type;
-
-    if (trigger.$type === 'MessageReceived') {
-        jsonTrigger['value'] = trigger.topic;
-    } else if (trigger.$type === 'TimerElapsed') {
-        jsonTrigger['value'] = trigger.timer;
-    } else if (trigger.$type === 'ServiceRequest') {
-        jsonTrigger['value'] = trigger.service;
-    } else if (trigger.$type === 'ActionGoalReceived') {
-        jsonTrigger['value'] = trigger.action;
-    } else if (trigger.$type === 'ParamChanged') {
-        jsonTrigger['value'] = trigger.param;
-    } else if (trigger.$type === 'StateChanged') {
-        jsonTrigger['value'] = trigger.state;
+function generateAction(
+    action: Action
+): { type: string; topic?: string; value?: number } | undefined {
+    if (action.$type === 'SendMessage') {
+        return { type: 'pub', topic: action.topic };
     }
 
-    return jsonTrigger;
+    if (action.$type === 'CallService' || action.$type === 'SendActionGoal') {
+        return { type: 'other', value: Number(action.expectedTime) };
+    }
+
+    return undefined;
 }
