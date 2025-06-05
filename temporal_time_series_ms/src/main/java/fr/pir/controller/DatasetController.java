bcbdaa796a5171dce7705978fef87cd82fbe38dc
Colin-de-Seroux <77831261+Colin-de-Seroux@users.noreply.github.com>
2025-06-05 09:12:43 +0200

    :sparkles: Creating dataset

---
+++ temporal_time_series_ms/src/main/java/fr/pir/controller/DatasetController.java
@@ -0,0 +1,56 @@
+package fr.pir.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.phenix333.logger.MyLogger;
+import fr.pir.service.DatasetService;
+
+@Controller
+@RequestMapping("/api/dataset")
+public class DatasetController {
+
+    private static final MyLogger L = MyLogger.create(DatasetController.class);
+
+    @Autowired
+    private DatasetService datasetService;
+
+    @GetMapping
+    public ResponseEntity<?> getDataset() {
+        L.function("");
+
+        try {
+            Path dataset = this.datasetService.getDataset();
+            Resource resource = new UrlResource(dataset.toUri());
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (IOException e) {
+            L.error("IO error while processing dataset", e);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(String.format("IO error while processing dataset: ", e.getMessage()));
+        } catch (Exception e) {
+            L.error("Error creating dataset", e);
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(String.format("Error creating dataset: ", e.getMessage()));
+        }
+    }
+
+}
---
+++ temporal_time_series_ms/src/main/java/fr/pir/service/DatasetService.java
@@ -0,0 +1,97 @@
+package fr.pir.service;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.phenix333.logger.MyLogger;
+import fr.pir.model.Action;
+import fr.pir.model.Behavior;
+import fr.pir.model.Model;
+import fr.pir.model.Node;
+import fr.pir.repository.ModelRepository;
+
+@Service
+public class DatasetService {
+
+    private static final MyLogger L = MyLogger.create(DatasetService.class);
+
+    @Autowired
+    private ModelRepository modelRepository;
+
+    public Path getDataset() throws IOException {
+        L.function("");
+
+        List<Model> models = modelRepository.findAll();
+
+        String outputFilePath = "dataset.csv";
+        String[] headers = {"ModelName", "NodeName", "BehaviorIndex", "ActionType", "ActionOrder", "Topic", "Value", "ExecutionTime"};
+
+        FileWriter writer = new FileWriter(outputFilePath);
+        CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader(headers));
+
+        int i = 0;
+
+        for (Model model : models) {
+            String[] row = new String[8];
+
+            int j = 0;
+            int x = 0;
+
+            for (Node node : model.getNodes()) {
+                for (Behavior behavior : node.getBehaviors()) {
+                    for (Action action : behavior.getActions()) {
+                        List<Double> executionTimes = action.getExecutionTimes();
+
+                        if (!executionTimes.isEmpty() && executionTimes.size() > x) {
+                            x = executionTimes.size();
+                        }
+                    }
+                }
+            }
+
+            for (int y = 0; y < x - 1; y++) {
+                for (Node node : model.getNodes()) {
+                    row[1] = node.getName() + "_" + j++;
+
+                    for (Behavior behavior : node.getBehaviors()) {
+                        row[2] = String.valueOf(behavior.getBehaviorIndex());
+
+                        for (Action action : behavior.getActions()) {
+                            row[0] = model.getName() + "_" + i;
+                            row[3] = action.getType();
+                            row[4] = String.valueOf(action.getActionOrder());
+                            row[5] = action.getTopic() != null ? action.getTopic() : "";
+                            row[6] = String.valueOf(action.getValue());
+
+                            try {
+                                row[7] = String.valueOf(action.getExecutionTimes().get(y));
+                            } catch (IndexOutOfBoundsException e) {
+                                row[7] = "0";
+                            }
+
+                            csvPrinter.printRecord((Object[]) row);
+                        }
+
+                    }
+                }
+
+                i++;
+            }
+        }
+
+        csvPrinter.flush();
+        csvPrinter.close();
+
+        return new File(outputFilePath).toPath().normalize();
+    }
+
+}
