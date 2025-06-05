ce165c3914d29c565b5194ed2f5db8dfa45200aa
Colin-de-Seroux <77831261+Colin-de-Seroux@users.noreply.github.com>
2025-06-05 23:00:54 +0200

    :see_no_evil: Oops, bad .gitignore

---
+++ temporal_time_series_ms/src/main/java/fr/pir/controller/LogsController.java
@@ -0,0 +1,47 @@
+package fr.pir.controller;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import fr.phenix333.logger.MyLogger;
+import fr.pir.service.LogsService;
+
+@Controller
+@RequestMapping("/api/logs")
+public class LogsController {
+
+    private static final MyLogger L = MyLogger.create(LogsController.class);
+
+    @Autowired
+    private LogsService logsService;
+
+    @PostMapping(consumes = {"multipart/form-data"})
+    public ResponseEntity<?> saveLogs(@RequestParam String modelName, @RequestPart("files") MultipartFile[] files) {
+        L.function("model name : {}, received files : {}", modelName, files.length);
+
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(this.logsService.saveLogs(modelName, files));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (IOException e) {
+            L.error("IO error while processing file", e);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid file format or content.");
+        } catch (Exception e) {
+            L.error("Error saving Logs", e);
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Oops, something went wrong.");
+        }
+    }
+
+}
