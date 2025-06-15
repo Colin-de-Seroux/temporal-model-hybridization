aefebe24cd7e2504b9a1fc3d0cebc118ce570dc8
Anna <annadiplacido3@gmail.com>
2025-06-15 15:27:28 +0200

    add forgotted path

--- ai/tgnn/tgnn_project/config.py
+++ ai/tgnn/tgnn_project/config.py
@@ -2,6 +2,7 @@ import os
     
 BASE_DIR = os.path.join("..", "dataset")
 DATASET_PATH = os.path.join(BASE_DIR, "dataset.csv")
+PREDICT_DATASET_PATH = os.path.join(BASE_DIR, "predict_dataset.csv")
 SAVE_DIR = os.path.join("..", "save")
 MODEL_PATH = os.path.join(SAVE_DIR, "model.pth")
 FEATURE_SCALER_PATH = os.path.join(SAVE_DIR, "feature_scaler.pkl")
