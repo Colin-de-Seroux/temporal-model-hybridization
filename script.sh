2c0692600856c3c6a0d9fe40c7ec7dad64ab0fa4
Colin-de-Seroux <77831261+Colin-de-Seroux@users.noreply.github.com>
2025-06-15 11:15:07 +0200

    :fire: Delete unused file

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run `docker compose up` 50 times in a row
-for i in {1..50}
-do
-    echo "Iteration $i: Starting docker compose up..."
-    docker compose up
-    echo "Iteration $i: Completed."
-done
